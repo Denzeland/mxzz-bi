6ac6ee0133a7ef5596443602761b20a1c625ed16
Denzeland <2426548297@qq.com>
2020-09-03 16:40:43 +0800

    删除mxzz-redash

--- mxzz-redash/viz-lib/src/visualizations/funnel/Editor/index.js
+++
@@ -1,9 +0,0 @@
-﻿import createTabbedEditor from "@/components/visualizations/editor/createTabbedEditor";
-
-import GeneralSettings from "./GeneralSettings";
-import AppearanceSettings from "./AppearanceSettings";
-
-export default createTabbedEditor([
-  { key: "General", title: "常规", component: GeneralSettings },
-  { key: "Appearance", title: "格式", component: AppearanceSettings },
-]);
